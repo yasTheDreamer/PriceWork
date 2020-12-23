d370e56534bbbd3a41512822d758dfd2f5fb6ab0
yasTheDreamer <yassine19265@gmail.com>
2020-12-23 18:56:21 +0100

    feature : implementing sessions

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -2,7 +2,11 @@ import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.post("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
+  console.log("on home page");
+});
+
+router.post("/", (req, res, next) => {
   // let user = new User()
   //   .withJobTitle("Full Stack Developer")
   //   .withSalary(3000)
